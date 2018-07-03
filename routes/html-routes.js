9eaec789ac1e185ade874d20d5691ef2e9fcb6bb
deasydoesit <darwin.rinderer@gmail.com>
2018-07-03 12:50:20 -0400

    Setting up handlebars and basic routing

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+var isAuthenticated = require("../config/middleware/isAuthenticated"); 
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    if (req.user) { //if user is logged in, render home page, otherwise render landing page
+      res.render("home");
+    }
+    res.render("landing");
+  });
+
+  app.get("/home", isAuthenticated, function(req, res) { //if user is logged in, render home page
+    res.render("home");
+  });
+
+};
