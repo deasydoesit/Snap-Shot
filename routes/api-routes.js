9ff92281333cf6e786bfd525091b304776199bc1
deasydoesit <darwin.rinderer@gmail.com>
2018-07-06 16:25:50 -0400

    Revising client-side spot creation form and server-side handling

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,42 +1,37 @@
 // var db = require("../models");
 // var passport = require("../config/passport");
 
+//Access Key ID:AKIAIV3PHOY5GHE23SXA
+//Secret Access Key: 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
+
+var aws = require('aws-sdk');
 var multer = require('multer');
+var multerS3 = require('multer-s3');
+
+aws.config.update({
+    secretAccessKey: 'hY9sXUk6PH6sY35kjBbMDlWrIYMQ0Y71THrIR5x2',
+    accessKeyId: 'AKIAIV3PHOY5GHE23SXA',
+    region: 'us-east-1'
+});
+
+var s3 = new aws.S3();
 
 module.exports = function(app) {
 
-  var multerConfig = {
-        storage: multer.diskStorage({
-        //Setup where the user's file will go
-        destination: function(req, file, next){
-        next(null, './public/photo-storage');
-        },   
-            //Then give the file a unique name
-            filename: function(req, file, next){
-                console.log(file);
-                const ext = file.mimetype.split('/')[1];
-                next(null, file.fieldname + '-' + Date.now() + '.'+ext);
-            }
-            }),   
-            //A means of ensuring only images are uploaded. 
-            fileFilter: function(req, file, next){
-                if(!file){
-                    next();
-                }
-                const image = file.mimetype.startsWith('image/');
-                if(image){
-                console.log('photo uploaded');
-                next(null, true);
-                }else{
-                console.log("file not supported");
-                //TODO:  A better message response to user on failure.
-                return next();
-                }
-            }
-    };
+  var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: "snap-spot",
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, file.fieldname + Date.now()); //use Date.now() for unique file keys
+        }
+    })
+  });
 
-  app.post("/upload", multer(multerConfig).single('photo'),function(req,res){
-    res.send('Complete!');
- });
+  app.post("/upload", upload.single('photo'), function (req, res, next) {
+    res.send("Uploaded!");
+    console.log(req.body);
+  });
 
 };
