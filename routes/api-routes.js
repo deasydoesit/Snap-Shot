a5569f214b8f858944c4cbed7298df3c90d8b10a
deasydoesit <darwin.rinderer@gmail.com>
2018-07-06 19:37:36 -0400

    Creating basic signup page with login navbar coupled to MySQL

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
 var aws = require('aws-sdk');
 var multer = require('multer');
 var multerS3 = require('multer-s3');
+var db = require("../models");
+var passport = require("../config/passport");
 
 aws.config.update({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -10,9 +12,7 @@ aws.config.update({
 
 var s3 = new aws.S3();
 
-module.exports = function(app) {
-
-  var upload = multer({
+var upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: "snap-spot",
@@ -23,9 +23,30 @@ module.exports = function(app) {
     })
   });
 
-  app.post("/upload", upload.single('photo'), function (req, res, next) {
-    res.send("Uploaded!");
-    console.log(req.body);
-  });
+module.exports = function(app) {
+
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json("test");
+    });
+
+    app.post("/api/signup", function(req, res) {
+        console.log(req.body);
+        db.User.create({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password
+        }).then(function() {
+            res.redirect(307, "/api/login");
+        }).catch(function(err) {
+            console.log(err);
+            res.json(err);
+        });
+    });
+
+    app.post("/api/upload", upload.single('photo'), function (req, res, next) {
+        res.send("Uploaded!");
+        console.log(req.body);
+    });
 
 };
